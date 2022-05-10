c1d822007985a79e9b5af5d5860e1f3fcfe7d7a6
Capotedev <carolina.capote@imagineapps.co>
2022-05-09 20:34:25 -0500

    UPDATE p id

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,8 +3,8 @@ import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<button type="button">Click here to get started</button>');
+$('body').append('<p id=count></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
@@ -12,4 +12,4 @@ function updateCounter() {
   $('#count').text(`${count} clicks on the button`);
 }
 
-$('button').on('click', _.debounce(updateCounter, 100));
+$('button').on('click', _.debounce(updateCounter, 300));
