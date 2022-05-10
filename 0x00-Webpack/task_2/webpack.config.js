e9a34d32b424371ee301ba28dd58e6b0811a0097
Capotedev <carolina.capote@imagineapps.co>
2022-05-09 23:30:10 -0500

    new test

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,13 +1,12 @@
-
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
 	entry: './js/dashboard_main.js',
 	output: {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'bundle.js'
 	},
+	mode: 'production',
 	module: {
 		rules: [
 			{
