88d8d489012a1b86b09142210d024beaa513de22
Capotedev <carolina.capote@imagineapps.co>
2022-05-11 22:20:41 -0500

    ADD files reorganization

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "../src/index.js",
   mode: "development",
   module: {
     rules: [
@@ -12,7 +12,7 @@ module.exports = {
         loader: "babel-loader",
         options: { presets: ["@babel/env"] },
         enforce: "pre",
-        use: ["source-map-loader"],
+        use: ["source-map-loader"]
       },
       {
         test: /\.css$/,
@@ -38,11 +38,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
+  
   plugins: [new webpack.HotModuleReplacementPlugin()]
 };
