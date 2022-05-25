2505e76a5be451b7aa18ea1e5c92deb0815aba8a
Capotedev <carolina.capote@imagineapps.co>
2022-05-24 22:20:15 -0500

    FIX tests

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -1,1 +1,12 @@
-// test file
+/**
+ * @jest-environment jsdom
+ */
+import { shallow } from "enzyme";
+import App from "./App";
+
+describe("<App/>", () => {
+  it("Tests App", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
